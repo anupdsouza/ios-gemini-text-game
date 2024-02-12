0ff2cff0ac1f9aeeba1a8c6b73632ae68f0d33ad
Anup DSouza <anup.dsouza@gmail.com>
2024-02-12 14:53:28 +0530

    Add initial files

---
+++ GeminiMurderMystery/GeminiMurderMystery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GeminiMurderMystery
+//
+//  Created by Anup D'Souza on 12/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GeminiMurderMystery/GeminiMurderMystery/GeminiMurderMysteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiMurderMysteryApp.swift
+//  GeminiMurderMystery
+//
+//  Created by Anup D'Souza on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GeminiMurderMysteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
